28ed2d73f3d0d7377d9f7ba4927bf776c4eadcb8
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-14 23:06:52 -0600

    other changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,6 @@
-root "/auth/facebook"
+Rails.application.routes.draw do
+  get 'users/index'
+  root 'users#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
