9de402000d26b1064bdbdb7e5227bdcc61aae7f6
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-14 20:08:10 -0600

    added some stuff

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module FacebookApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Koala.config.api_version = 'v2.0'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,1 @@
-Rails.application.routes.draw do
-  get 'main_page/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+root "/auth/facebook"
