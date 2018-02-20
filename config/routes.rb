f1074c237231dce93bf65b5b58a740b554f188ae
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-19 21:09:29 -0600

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  root 'users#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root to: 'users#index', via: :get
+ get 'auth/facebook', as: "auth_provider"
+ get 'auth/facebook/callback', to: 'users#login'
 end
