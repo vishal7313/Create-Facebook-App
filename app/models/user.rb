2ce237ffbbef18a6f673097fbdc7c51fdf44cbd0
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-21 15:54:40 -0600

    working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   def self.koala(auth)
     access_token = auth['token']
     facebook = Koala::Facebook::API.new(access_token)
-    facebook.get_object("me?fields=name,picture")
+    facebook.get_object("me?fields=name,picture,movies")
   end
 
 end
