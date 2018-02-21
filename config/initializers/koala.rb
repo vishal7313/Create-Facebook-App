5fd1654f0aa641dc9a91f673e0e62fe65a16d301
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-21 15:25:42 -0600

    fixes

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -3,7 +3,7 @@ Koala.configure do |config|
   config.access_token = ENV['TOKEN']
   config.app_access_token = ENV['APP_ACCESS_TOKEN']
   config.app_id = ENV['APP_ID']
-  config.app_secret = ENV['secret']
+  config.app_secret = ENV['FACEBOOK_SECRET']
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
