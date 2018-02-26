8d6bb4d4aa7966d61f0753eef9471bf51ec41be9
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-25 23:50:15 -0600

    more info per description pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def index
   end
-
+  def ruby_guide
+  end
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  root to: 'users#index', via: :get
  get 'auth/facebook', as: "auth_provider"
  get 'auth/facebook/callback', to: 'users#login'
+ get 'ruby/details', to: 'users#ruby', as: 'ruby_guide'
 end
