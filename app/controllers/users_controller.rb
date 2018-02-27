db571597296d67fc7698612fc8343c2c4ec5c0e3
terrancehanlon <terrance.hanlon@ndsu.edu>
2018-02-26 22:50:34 -0600

    added new view for facebook app info page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   end
   def ruby_guide
   end
+
+  def app_info
+  end
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
  get 'auth/facebook', as: "auth_provider"
  get 'auth/facebook/callback', to: 'users#login'
  get 'ruby/details', to: 'users#ruby', as: 'ruby_guide'
+ get 'app/info', to: 'users#facebook_app_info', as: 'app_info'
 end
